008a7dcdd228004a4c9bba74a862c53c9089fad3
badumbatish <tanghocle456@gmail.com>
2024-12-05 00:11:38 -0800

    Pass manager wrapper for lowering

--- shuriken/mjolnir/include/transform/lower.h
+++ shuriken/mjolnir/include/transform/lower.h
@@ -1,14 +1,14 @@
 
 
-#ifndef LIFTER_MJOLNIR_LIFTER_HPP
-#define LIFTER_MJOLNIR_LIFTER_HPP
+#ifndef LOWER_MJOLNIR_LOWER_HPP
+#define LOWER_MJOLNIR_LOWER_HPP
 
 #include "mjolnir/MjolnIRDialect.h"
 
+#include "dalvik/DalvikPatterns.h.inc"
 #include "mjolnir/MjolnIROps.h"
 #include "mjolnir/MjolnIRTypes.h"
-
-#include "dalvik/DalvikPatterns.h.inc"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/PatternMatch.h"
 /// MLIR includes
 #include <llvm/Support/Casting.h>
@@ -21,26 +21,28 @@
 #include <mlir/IR/BuiltinOps.h>
 #include <mlir/IR/Dialect.h>
 #include <mlir/IR/MLIRContext.h>
+#include <mlir/IR/OperationSupport.h>
 #include <mlir/Interfaces/FunctionInterfaces.h>
 #include <mlir/Pass/Pass.h>
+#include <mlir/Pass/PassManager.h>
 #include <mlir/Support/LLVM.h>
-using namespace mlir;
+#include <mlir/Transforms/GreedyPatternRewriteDriver.h>
 namespace shuriken {
     namespace MjolnIR {
         class LowerAdd : public mlir::OpRewritePattern<mlir::arith::AddIOp> {
             using LogicalResult = llvm::LogicalResult;
-            LowerAdd(MLIRContext *context) : OpRewritePattern(context, /*benefit=*/3) {}
+            LowerAdd(mlir::MLIRContext *context) : OpRewritePattern(context, /*benefit=*/3) {}
 
-            LogicalResult matchAndRewrite(arith::AddIOp op, mlir::PatternRewriter &rewriter) const override {
+            LogicalResult matchAndRewrite(mlir::arith::AddIOp op, mlir::PatternRewriter &rewriter) const override {
                 auto rhs = op.getRhs();
-                if (llvm::isa<arith::ConstantOp>(rhs)) return llvm::success();
+                if (llvm::isa<mlir::arith::ConstantOp>(rhs)) return llvm::success();
                 else
                     return llvm::success();
             }
         };
         class LowerFallThrough : public mlir::OpRewritePattern<mlir::shuriken::MjolnIR::FallthroughOp> {
             using LogicalResult = llvm::LogicalResult;
-            LowerFallThrough(MLIRContext *context) : OpRewritePattern(context, /*benefit=*/1) {}
+            LowerFallThrough(mlir::MLIRContext *context) : OpRewritePattern(context, /*benefit=*/1) {}
 
             LogicalResult matchAndRewrite(mlir::shuriken::MjolnIR::FallthroughOp op, mlir::PatternRewriter &rewriter) const override {
                 rewriter.eraseOp(op);
@@ -48,9 +50,39 @@ namespace shuriken {
             }
         };
 
-
         /// The Lower class applies all lowering patterns that is added to it when it enters/encounters a FuncOp
-        class Lower : public mlir::PassWrapper<Lower, OperationPass<mlir::func::FuncOp>> {};
+        class DalvikLower : public mlir::PassWrapper<DalvikLower, mlir::OperationPass<mlir::func::FuncOp>> {
+
+        private:
+            void registerMjolnIRToDalvikLowering(mlir::MLIRContext &context, mlir::RewritePatternSet &s) {
+                s.add<LowerFallThrough>(&context);
+            }
+            void runOnOperation() override {
+                mlir::func::FuncOp func = getOperation();
+                mlir::MLIRContext &context = getContext();
+
+                // Create a RewritePatternSet and add your patterns
+                mlir::RewritePatternSet patterns(&context);
+                registerMjolnIRToDalvikLowering(context, patterns);
+
+                // Apply the patterns to the function
+                if (mlir::failed(mlir::applyPatternsAndFoldGreedily(func, std::move(patterns)))) {
+                    signalPassFailure();
+                }
+            }
+        };
+
+        class Lower {
+        public:
+            Lower() {
+                mlir::MLIRContext context;
+                // Load required dialects
+                context.getOrLoadDialect<mlir::shuriken::MjolnIR::MjolnIRDialect>();
+                context.getOrLoadDialect<mlir::func::FuncDialect>();
+                mlir::PassManager pm(&context);
+                pm.addPass(std::make_unique<DalvikLower>());
+            }
+        };
     }// namespace MjolnIR
 }// namespace shuriken
-#endif// LIFTER_MJOLNIR_LIFTER
+#endif// LOWER_MJOLNIR_LOWER
