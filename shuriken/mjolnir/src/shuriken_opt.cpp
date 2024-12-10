211453d30265698fdcbc3a00f74cdf7ccd353b7f
badumbatish <tanghocle456@gmail.com>
2024-12-10 01:55:53 -0800

    Modify cmdline opt for better control of different phases

--- shuriken/mjolnir/src/shuriken_opt.cpp
+++ shuriken/mjolnir/src/shuriken_opt.cpp
@@ -30,12 +30,19 @@ int main(int argc, char **argv) {
             {"--file", ""}};
 
 
+    LOGGING = std::find_if(args.begin(), args.end(), [](auto &arg) { return arg == "-d" || arg == "--diagnostics"; }) != args.end();
     // INFO: Check if we need to print out help
     bool need_help = std::find_if(args.begin(), args.end(), [](auto &arg) { return arg == "-h" || arg == "--help"; }) != args.end();
-    LOGGING = std::find_if(args.begin(), args.end(), [](auto &arg) { return arg == "-d" || arg == "--diagnostics"; }) != args.end();
     bool show_graph = std::find_if(args.begin(), args.end(), [](auto &arg) { return arg == "-g" || arg == "--graph"; }) != args.end();
+    bool lift = std::find_if(args.begin(), args.end(), [](auto &arg) { return arg == "--lift"; }) != args.end();
+    bool lower = std::find_if(args.begin(), args.end(), [](auto &arg) { return arg == "--lower"; }) != args.end();
+    bool opt = std::find_if(args.begin(), args.end(), [](auto &arg) { return arg == "--opt"; }) != args.end();
+
     if (LOGGING)
         shuriken_opt_log(fmt::format("\nLOGGING IS ENABLED\n"));
+
+    // INFO: Lowering or optimization requires lifting
+    if (lower || opt) lift = true;
 
     // INFO: Error is true if something is wrong with input
     bool error = acquire_input(args, options);
@@ -52,31 +59,40 @@ int main(int argc, char **argv) {
 
     /// INFO: Process a file.
     if (options.at("-f") != "" || options.at("--file") != "") {
+
         std::string file_name = options.at("-f") != "" ? options.at("-f") : options.at("--file");
         shuriken_opt_log(fmt::format("The file name is {}\n", file_name));
-        std::cerr << "*****BEGIN LIFTING*****\n";
-        shuriken::MjolnIR::Lifter lifter(file_name, false, true);
-        for (auto &module: lifter.mlir_gen_result) { module->dump(); }
+        if (lift) {
+            std::cerr << "*****BEGIN LIFTING*****\n";
+            shuriken::MjolnIR::Lifter lifter(file_name, false, true);
+            for (auto &module: lifter.mlir_gen_result) { module->dump(); }
 
-        if (show_graph) {
+            if (show_graph) {
 
-            for (auto &module: lifter.mlir_gen_result) {
-                auto logical_result = shuriken::MjolnIR::generate_functions_cfg(llvm::outs(), module);
-                if (logical_result.failed())
-                    return -1;
+                for (auto &module: lifter.mlir_gen_result) {
+                    auto logical_result = shuriken::MjolnIR::generate_functions_cfg(llvm::outs(), module);
+                    if (logical_result.failed())
+                        return -1;
+                }
             }
-        }
-        std::cerr << "*****BEGIN Optimizer*****\n";
-        shuriken::MjolnIR::Opt opt;
-        for (auto &module: lifter.mlir_gen_result) {
-            auto result = opt.run(module.get());
-            if (mlir::failed(result)) {
-                llvm::errs() << "Failed to optimize" << module.get().getName() << "\n";
-            } else {
-                module->dump();
+            if (opt) {
+                std::cerr << "*****BEGIN Optimizer*****\n";
+                shuriken::MjolnIR::Opt opt;
+                for (auto &module: lifter.mlir_gen_result) {
+                    auto result = opt.run(module.get());
+                    if (mlir::failed(result)) {
+                        llvm::errs() << "Failed to optimize" << module.get().getName() << "\n";
+                    } else {
+                        module->dump();
+                    }
+                }
             }
-            break;
+
+            if (lower) {}
+        } else {
+            std::cerr << "USer picked the -f | --file option but did not specify at least lift | lower | opt\n";
         }
+
         return 0;
     }
 
@@ -86,10 +102,13 @@ int main(int argc, char **argv) {
 }
 
 void show_help(std::string &prog_name) {
-    fmt::print(fg(fmt::color::green), "USAGE: {} [-h | --help] [-d | --diagnostics] [-f|--file file_name] \n", prog_name);
+    fmt::print(fg(fmt::color::green), "USAGE: {} [-h | --help] [-d | --diagnostics] [-f|--file file_name --lift --lower --opt] \n", prog_name);
     fmt::print(fg(fmt::color::green), "    -h | --help: Shows the help menu, like what you're seeing right now\n");
     fmt::print(fg(fmt::color::green), "    -d | --diagnostics: Enables diagnostics for shuriken-opt\n");
-    fmt::print(fg(fmt::color::green), "    -f | --file: Analyzes a file with file name\n");
+    fmt::print(fg(fmt::color::green), "    -f | --file: Analyzes a file with file name, needs additional information\n");
+    fmt::print(fg(fmt::color::green), "    -lift : Lift the dex format up to MjolnIR\n");
+    fmt::print(fg(fmt::color::green), "    -lower: Lower the lifted MjolnIR down to smali (Enables lift when this is opted)\n");
+    fmt::print(fg(fmt::color::green), "    -opt: Run some default optimization (Nop removal for now)\n");
     fmt::print(fg(fmt::color::green), "    -g | --graph: Dump the graph in dot format (needs a file)\n");
 }
 /// Simple log for shuriken opt, msgs need to provide newline.
