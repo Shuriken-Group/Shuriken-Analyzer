b6f8624765b91f9c3f2cfe4b51d0f1a621272dd5
badumbatish <tanghocle456@gmail.com>
2024-12-10 00:53:55 -0800

    let opt go into passes, mjolnir to smali into transform

---
+++ shuriken/mjolnir/include/transform/mjolnir_to_smali.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <mlir/Dialect/Arith/IR/Arith.h>
+#include <mlir/Dialect/ControlFlow/IR/ControlFlow.h>
+#include <mlir/Dialect/ControlFlow/IR/ControlFlowOps.h>
+#include <mlir/IR/BuiltinOps.h>
+#include <mlir/IR/OwningOpRef.h>
+#include <mlir/Support/LLVM.h>
+#include <string>
+#include <vector>
+namespace shuriken::MjolnIR {
+    std::vector<std::string> to_smali(std::vector<mlir::OwningOpRef<mlir::ModuleOp>> &modules);
+}// namespace shuriken::MjolnIR
